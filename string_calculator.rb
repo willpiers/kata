9b0aabe81a5c9df18e55666c27a920d6ba9a4b8d
Will Piers <wpiers0405@gmail.com>
2013-11-13 17:31:11 -0700

    finisehd steps 2 and 3. calculator now handles arbitrarily many arguments and tolerates whitespace

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,11 @@
 class StringCalculator
-	def add(summands)
-		return 0 if summands.empty?
+	def self.add summand_string
+		return 0 if summand_string.empty?
+		summands = homogenize summand_string
 		summands.split(',').map {|summand| summand.to_i}.reduce(:+)
+	end
+
+	def self.homogenize summand_string
+		summand_string.gsub(/\s/, ',')
 	end
 end
