94ed3a2e54d35a1403e05134ec3b545a0539e5d8
Will Piers <wpiers0405@gmail.com>
2013-11-16 20:40:22 -0700

    converted indentation to spaces.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,41 +1,41 @@
 require_relative '../string_calculator'
 
 describe StringCalculator do
-	it "should have an add instance method" do
-		StringCalculator.should respond_to :add
-	end
+  it "should have an add instance method" do
+    StringCalculator.should respond_to :add
+  end
 
-	describe StringCalculator, '.add' do
-		it "should return 0 if given no summands" do
-		  StringCalculator.add('').should eq 0
-		end
+  describe StringCalculator, '.add' do
+    it "should return 0 if given no summands" do
+      StringCalculator.add('').should eq 0
+    end
 
-		it "should return the integer given if only given one" do
-		  StringCalculator.add('2').should eq 2
-		end
+    it "should return the integer given if only given one" do
+      StringCalculator.add('2').should eq 2
+    end
 
-		it "should be able to add two integers" do
-		  StringCalculator.add('2,3').should eq 5
-		end
+    it "should be able to add two integers" do
+      StringCalculator.add('2,3').should eq 5
+    end
 
-		it "should handle more than 2 summands" do
-			StringCalculator.add('2,3,4,5,1000').should eq 1014
-		end
+    it "should handle more than 2 summands" do
+      StringCalculator.add('2,3,4,5,1000').should eq 1014
+    end
 
-		it "should be able to handle new lines" do
-		  StringCalculator.add("2\n3").should eq 5
-		end
-	end
+    it "should be able to handle new lines" do
+      StringCalculator.add("2\n3").should eq 5
+    end
+  end
 
-	describe '.homogenize' do
-		it "should replace newline characters with commas" do
-			StringCalculator.homogenize("2\n3").should eq "2,3"
-		end
-	end
+  describe '.homogenize' do
+    it "should replace newline characters with commas" do
+      StringCalculator.homogenize("2\n3").should eq "2,3"
+    end
+  end
 
-	describe '.find_delimeter' do
-		it "should find a delimeter at the beginning of the summand string" do
-		  StringCalculator.find_delimeter("//hello\n23hello4").should eq 'hello'
-		end
-	end
+  describe '.find_delimeter' do
+    it "should find a delimeter at the beginning of the summand string" do
+      StringCalculator.find_delimeter("//hello\n23hello4").should eq 'hello'
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,31 +1,33 @@
 class StringCalculator
-	def self.add summand_string
-		return 0 if summand_string.empty?
+  def self.add summand_string
+    return 0 if summand_string.empty?
 
-		delimeter = find_delimeter summand_string
+    delimeter = find_delimeter summand_string
 
-		summands = if delimeter
-			homogenize(summand_string, delimeter)
-		else
-			homogenize(summand_string)
-		end
+    summands = if delimeter
+      homogenize(summand_string, delimeter)
+    else
+      homogenize(summand_string)
+    end
 
-		summands.split(',').map {|summand| summand.to_i}.reduce(:+)
-	end
+    summands.split(',').map do |summand|
+      summand.to_i
+    end.reduce(:+)
+  end
 
-	def self.homogenize summand_string, delimeter=nil
-		summand_string.gsub!(/\s/, ',')
-		if delimeter
-			summand_string.gsub!(delimeter, ',')
-		end
-		summand_string
-	end
+  def self.homogenize summand_string, delimeter=nil
+    summand_string.gsub!(/\s/, ',')
+    if delimeter
+      summand_string.gsub!(delimeter, ',')
+    end
+    summand_string
+  end
 
-	def self.find_delimeter summand_string
-		first_line = summand_string.split(/\n/).first
-		if first_line[0..1] == '//'
-			return first_line[2..-1]
-		end
-		nil
-	end
+  def self.find_delimeter summand_string
+    first_line = summand_string.split(/\n/).first
+    if first_line[0..1] == '//'
+      return first_line[2..-1]
+    end
+    nil
+  end
 end
